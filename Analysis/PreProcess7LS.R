81afc6fc7c6242c6ccba577f0ebd34fb382a43ac
Harry Steinharter <harrysteinharter@ip-145-116-186-198.wlan-int.ru.nl>
2025-05-13 14:25:17 +0200

    Analysis Progress

--- Analysis/PreProcess7LS.R
+++ Analysis/PreProcess7LS.R
@@ -1,28 +1,98 @@
 library(magrittr)
 library(dplyr)
+
+MC <- function(L_max, L_min = 112.6482, nDigits=3){
+  # Function to find michelson contrast using L_min as the background luminance in PsychoPy units
+  michelson_contrast <- (L_max-L_min) / (L_max+L_min)
+  michelson_contrast %<>% round(nDigits)
+  return(michelson_contrast)
+}
+
+toCandela <- function(x, nDigits = 10){
+  CandelaPerMeterSquared <- (x * 112.417) + 112.6482
+  CandelaPerMeterSquared %<>% round(nDigits)
+  return(CandelaPerMeterSquared)
+}
+
+categorize <- function(df){
+  df %<>%mutate(
+    WidthGroup = case_when(
+      CondShort %in% c("Constant", "Single") ~ "controls",
+      CondShort %in% c('IW','DW') ~ "wide",
+      CondShort %in% c('IN','DN') ~ "narrow"
+    ),
+    ExpectedGroup = case_when(
+      CondShort %in% c('Constant','IN','IW') ~ 'high',
+      CondShort %in% c('Single','DN','DW') ~ 'low'
+    )
+  )
+  return(df)
+}
+
 setwd('/Users/harrysteinharter/Documents/MSc/Timo Internship/7LineSegments/Analysis')
 # List all CSV files in the directory
 file_list <- list.files(path = "../Outputs/", pattern = "\\.csv$", full.names = TRUE)
 # Read and bind all CSV files into one data frame
 all_data <- do.call(rbind, lapply(file_list, read.csv))
+df <- all_data %>%  subset(Reaction_Time < 99)
+
+
+#### Perform transformations ####
+if (TRUE){
+  df$TC_c <- scale(df$Target_Contrast,scale=F) %>% c()
+  df$TC_michelson <- toCandela(df$Target_Contrast) %>% MC()
+  df$TC_michelson_c <- scale(df$TC_michelson,scale=F) %>% c()
+  
+  df$FCfar_c <- scale(df$FCfar,scale=F) %>% c()
+  df$FCfar_michelson <- toCandela(df$FCfar) %>% MC()
+  df$FCfar_michelson_c <- scale(df$FCfar_michelson,scale=F) %>% c()
+  
+  df$FCmed_c <- scale(df$FCmed,scale=F) %>% c()
+  df$FCmed_michelson <- toCandela(df$FCmed) %>% MC()
+  df$FCmed_michelson_c <- scale(df$FCmed_michelson,scale=F) %>% c()
+  
+  df$FCclose_c <- scale(df$FCclose,scale=F) %>% c()
+  df$FCclose_michelson <- toCandela(df$FCclose) %>% MC()
+  df$FCclose_michelson_c <- scale(df$FCclose_michelson,scale=F) %>% c()
+  
+  df$CondShort <- recode(df$Condition, recodes = 
+                           "'FarHighNarrow' = 'IN'; 'FarLowNarrow' = 'DN';
+                        'FarHighWide' = 'IW'; 'FarLowWide' = 'DW';
+                        'ThreeLinesControl' = 'Single'; 'Constant' = 'Constant'") %>% as.factor()
+  
+  df %<>% mutate(TC_factor = ggplot2::cut_number(Target_Contrast, 3, labels = c("Low", "Medium", "High")))
+}
+#### Seperate ####
 # Seperate into Real and Null Files
-dfNull <- subset(all_data, endsWith(Condition,"_null"))
-dfReal <- subset(all_data, !endsWith(Condition,"_null"))
+dfNull <- subset(df, endsWith(Condition,"_null"))
+droplevels(dfNull$CondShort)
+dfReal <- subset(df, !endsWith(Condition,"_null"))
+droplevels(dfReal$CondShort)
+dfReal %<>% categorize
+
+#### Mutate real data ####
+grandMean_real <- dfReal %>% 
+  group_by(Participant_Number,Condition) %>% 
+  mutate(overall_mean = mean(TC_michelson), weights = n()) %>% 
+  select(Participant_Number,CondShort,overall_mean,WidthGroup,ExpectedGroup) %>% 
+  ungroup() %>% 
+  distinct()
+
+meanByTC <- dfReal %>% 
+  group_by(Participant_Number,CondShort,TC_factor) %>% 
+  mutate(mean = mean(TC_michelson), weights = n()) %>% 
+  select(Participant_Number,TC_factor,CondShort,mean,WidthGroup,ExpectedGroup) %>% 
+  ungroup() %>% 
+  distinct()
+
+rawIndividual <- dfReal %>% 
+  group_by(Participant_Number,CondShort,Target_Contrast) %>% 
+  mutate(performance = mean(Correct_Response), weights = n()) %>% 
+  ungroup() %>% 
+  distinct()
 
 # Save the raw data
-write.csv(dfReal,'rawData7LS.csv', row.names=FALSE)
+write.csv(rawIndividual,'glmDataRaw7LS.csv', row.names=FALSE)
+write.csv(meanByTC,'subjectMeansByTCFactorRaw7LS.csv',row.names = FALSE)
+write.csv(grandMean_real,'subjectMeansOverallRaw7LS.csv',row.names = FALSE)
 
-# Calculate false-positive rate
-FPs <- c()
-
-dfNull$Condition %<>% as.factor
-dfReal$Condition %<>% as.factor
-condsNull <- levels(dfNull$Condition)
-condsReal <- levels(dfReal$Condition)
-
-people <- dfReal$Participant_Number %>% unique
-
-df_summary <- dfNull %>% 
-  group_by(Participant_Number, Condition) %>% 
-  summarise(false_positive_rate = mean(Correct_Response, na.rm = TRUE)) %>% 
-  ungroup()
